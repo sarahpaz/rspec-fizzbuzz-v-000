2ca4809b04ecca608d26f2335887e81b4aeea983
Sarah Paz <sarah.paz@gmail.com>
2018-11-22 13:52:21 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,8 +4,15 @@ def fizzbuzz(int)
   if int % 3 == 0
     "Fizz"
   end
+
+  if int % 5 == 0
+    "Buzz"
+  end
+
+  if int % 15 == 0
+    "Fizzbuzz"
 end
 
 fizzbuzz(3)
 fizzbuzz(5)
-fizzbuzz()
+fizzbuzz(4)
