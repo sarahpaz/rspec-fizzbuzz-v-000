d7037f49bcef20feec2662cacbce5e8f288d60ff
Sarah Paz <sarah.paz@gmail.com>
2018-11-22 13:55:56 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,16 +1,17 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
-  if int % 3 == 0
+  if (int % 3 == 0)
     "Fizz"
-  end
 
-  if int % 5 == 0
+  elsif (int % 5 == 0)
     "Buzz"
-  end
 
-  if int % 15 == 0
+  elsif (int % 15 == 0)
     "Fizzbuzz"
+
+  else (inf !% 3 || 5)
+    "Nil"
   end
 end
 
