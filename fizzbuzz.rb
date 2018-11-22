f051c6214e04db45790d31d836e2df28e474785c
Sarah Paz <sarah.paz@gmail.com>
2018-11-22 14:13:06 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,21 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-require 'pry'
 
 def fizzbuzz(int)
-  binding.binding.pry
   if int % 3 == 0 # if the number int is divisible by 3
     "Fizz" # Go fizz
-  elsif int % 5 == 0
-    "Buzz"
-  elsif int % (3 && 5)
-    "FizzBuzz"
-  else
-    "nil"
   end
 end
 
-fizzbuzz(3)
-fizzbuzz(5)
-fizzbuzz(15)
-fizzbuzz(4)
+fizzbuzz(3) # => You should see a return of "Fizz"
+fizzbuzz(5) # => You should see a return of nil
+fizzbuzz()  # => You should get an ArgumentError
