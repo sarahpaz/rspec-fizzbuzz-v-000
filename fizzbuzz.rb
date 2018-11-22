17156e8113b456a89daae68466027d7e1b3f36aa
Sarah Paz <sarah.paz@gmail.com>
2018-11-22 13:59:03 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,17 +1,7 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
-  if int % 3 == 0
-    puts "Fizz"
-  elsif int % 5 == 0
-    puts "Buzz"
-  elsif int % 15 == 0
-    puts "Fizzbuzz"
-  else inf !% 3 || 5
-    puts "nil"
+  if int % 3 == 0 # if the number int is divisible by 3
+    "Fizz" # Go fizz
+  end
 end
-
-fizzbuzz(3)
-fizzbuzz(5)
-fizzbuzz(15)
-fizzbuzz(4)
