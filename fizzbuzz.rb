e08eb6a425b3adef48affe6289f3e3fa6682a4b4
Sarah Paz <sarah.paz@gmail.com>
2018-11-22 14:07:00 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,10 +5,12 @@ def fizzbuzz(int)
     "Fizz" # Go fizz
   elsif int % 5 == 0
     "Buzz"
-  elsif int % 3 || 5 == 0
+  elsif int % 3 && 5 == 0
     "FizzBuzz"
-  else
+  elsif int !% 3 || 5 == 0
     "nil"
+  else
+    
   end
 end
 
